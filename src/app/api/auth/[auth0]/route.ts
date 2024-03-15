b521c1a5dcf0a117d21aba99a27263b30032c12c
logiclynxAJ <dev.jha.arya@gmail.com>
2024-03-15 18:05:45 +0530

    Enabled edge for auth0

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,7 +1,9 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0/edge";
 
 export const GET = handleAuth({
   signup: handleLogin({
     authorizationParams: { screen_hint: "signup" },
   }),
 });
+
+export const runtime = "edge";
