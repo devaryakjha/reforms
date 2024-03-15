a8365baecad7735275d53a3c8ca2fe663d962332
logiclynxAJ <dev.jha.arya@gmail.com>
2024-03-16 02:53:57 +0530

    WIP CSP

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,43 +19,38 @@ export const config = {
   ],
 };
 
-export function middleware(request: NextRequest) {
+export function middleware(_: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`;
-  // Replace newline characters and spaces
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, " ")
-    .trim();
+      default-src 'self';
+      script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' ${
+        process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+      };
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data:;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      block-all-mixed-content;
+      upgrade-insecure-requests;
+  `;
 
-  const requestHeaders = new Headers(request.headers);
+  const requestHeaders = new Headers();
+
+  // Setting request headers
   requestHeaders.set("x-nonce", nonce);
-
   requestHeaders.set(
     "Content-Security-Policy",
-    contentSecurityPolicyHeaderValue,
+    // Replace newline characters and spaces
+    cspHeader.replace(/\s{2,}/g, " ").trim(),
   );
 
-  const response = NextResponse.next({
+  return NextResponse.next({
+    headers: requestHeaders,
     request: {
       headers: requestHeaders,
     },
   });
-  response.headers.set(
-    "Content-Security-Policy",
-    contentSecurityPolicyHeaderValue,
-  );
-
-  return response;
 }
