827ea80e918d4b4f2a4cd5f5d952e26ce0177652
logiclynxAJ <dev.jha.arya@gmail.com>
2024-03-16 02:28:50 +0530

    Added nonce based CSP

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,3 @@
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -21,19 +7,6 @@ const nextConfig = {
         hostname: "lh3.googleusercontent.com",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
   },
 };
 
